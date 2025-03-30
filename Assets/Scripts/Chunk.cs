cf296c4a9f56c39f514feec64de8507f27ee2cfa
myth0629 <84004727+myth0629@users.noreply.github.com>
2025-03-31 00:03:03 +0900

    장애물, 사과, 코인 생성
    120.

---
+++ Assets/Scripts/Chunk.cs
@@ -0,0 +1,86 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour
+{
+    [SerializeField] GameObject fencePrefab; // 장애물 프리팹
+    [SerializeField] GameObject applePrefab; 
+    [SerializeField] GameObject coinPrefab; 
+    [SerializeField] float appleSpawnChance = 0.5f; // 사과 생성 확률
+    [SerializeField] float coinSpawnChance = 0.5f; // 동전 생성 확률
+    [SerializeField] float[] lanes = { -2.5f, 0f, 2.5f }; // 장애물 생성 위치 (3개)
+    [SerializeField] float coinSeperationLength = 2f;
+    
+    List<int> availableLanes = new List<int>{0, 1, 2}; // 사용 가능한 장애물 위치를 저장할 리스트
+
+    void Start()
+    {
+        SpawnFences();
+        SpawnApple();
+        SpawnCoin();
+    }
+
+    void SpawnFences()
+    {
+        int fencesToSpawn = Random.Range(0, lanes.Length); // 0~2개 장애물 생성
+
+        for(int i = 0; i < fencesToSpawn; i++)
+        {
+            if (availableLanes.Count == 0) // 사용 가능한 장애물 위치가 없으면 종료
+            {
+                break;
+            }
+
+            int randomLaneIndex = SelectLane();
+
+            Vector3 spawanPosition = new Vector3(lanes[randomLaneIndex], transform.position.y, transform.position.z); // 장애물 생성 위치
+            Instantiate(fencePrefab, spawanPosition, Quaternion.identity, transform); // 장애물 생성
+            // transform을 붙여야 장애물이 Chunk의 자식으로 생성되어 같이 움직임 
+        }
+    }
+
+    int SelectLane()
+    {
+        int randomLaneIndex = Random.Range(0, availableLanes.Count); // 0~2 랜덤 생성
+        int selectedLane = availableLanes[randomLaneIndex]; // 랜덤으로 선택된 장애물 위치
+        availableLanes.RemoveAt(randomLaneIndex); // 선택된 장애물 위치는 리스트에서 제거
+        return selectedLane; // 선택된 장애물 위치 반환
+    }
+
+    void SpawnApple()
+    {
+        if (Random.value > appleSpawnChance || availableLanes.Count <= 0) // 랜덤으로 사과 생성 여부 결정
+        {
+            return;
+        }
+
+        int randomLaneIndex = SelectLane();
+
+        Vector3 spawanPosition = new Vector3(lanes[randomLaneIndex], transform.position.y, transform.position.z); // 장애물 생성 위치
+        Instantiate(applePrefab, spawanPosition, Quaternion.identity, transform); // 장애물 생성
+    }
+
+    void SpawnCoin()
+    {
+        if (Random.value > coinSpawnChance || availableLanes.Count <= 0) // 랜덤으로 코인 생성 여부 결정
+        {
+            return;
+        }
+
+        int randomLaneIndex = SelectLane();
+
+        int maxCoinsToSpawn = 6;
+        int coinsToSpawn = Random.Range(1, maxCoinsToSpawn); 
+
+        float topOfChunkZPos = transform.position.z + (coinSeperationLength * 2f); // 코인 생성 위치 (장애물 위쪽)
+
+        for (int i = 0; i < coinsToSpawn; i++)
+        {
+            float spawnPositionZ = topOfChunkZPos - (coinSeperationLength * i);
+
+            Vector3 spawanPosition = new Vector3(lanes[randomLaneIndex], transform.position.y, spawnPositionZ); // 코인 생성 위치
+            Instantiate(coinPrefab, spawanPosition, Quaternion.identity, transform); // 코인 생성
+        }
+
+    }
+}
--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class ObstacleSpawner : MonoBehaviour
 {
-    [SerializeField] GameObject obstaclePrefab;
+    [SerializeField] GameObject[] obstaclePrefabs;
     [SerializeField] float spawnTime = 1f; // 장애물 생성 주기
+    [SerializeField] Transform obstacleParent;
+    [SerializeField] float spawnWidth = 4f; // 장애물 생성 범위
 
     void Start()
     {
@@ -16,8 +18,11 @@ public class NewMonoBehaviourScript : MonoBehaviour
     {
         while(true)
         {
+            // 장애물 프리팹 중 랜덤으로 선택하여 생성
+            GameObject obstaclePrefab = obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)];
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnWidth, spawnWidth), transform.position.y, transform.position.z);
             yield return new WaitForSeconds(spawnTime); // 1초 대기
-            Instantiate(obstaclePrefab, transform.position, Random.rotation);
+            Instantiate(obstaclePrefab, spawnPosition, Random.rotation, obstacleParent);
         }
     }
 }
